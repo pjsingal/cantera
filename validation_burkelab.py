708afbf5bd524a08baad859b2986f79440ed6632
Patrick Singal <pjs2206@columbia.edu>
2023-11-14 15:38:03 -0500

    updates_nov14

--- validation_burkelab.py
+++ validation_burkelab.py
@@ -1,7 +1,32 @@
 #Validation script
 import sys, os
-sys.path.append("C:\\Users\\pjsin\\Documents\\cantera\\build\\python")
+sys.path.append("C:/Users/pjsin/Documents/cantera/build/python")
 import cantera as bklabct
 
 gas = bklabct.Solution('C:\\Users\\pjsin\\Documents\\cantera\\test\\data\\kineticsfromscratch_LMRtest.yaml')
-print(gas.species_names)
+gas.TPX= 1000, bklabct.one_atm, {"H2O":1}
+# reaction = "H + O2 <=> HO2"
+# Rc1=gas.forward_rate_constants[gas.reaction_equations().index(reaction)]
+# print(Rc1)
+
+# Create a reactor
+reactor = bklabct.IdealGasReactor(gas)
+
+# Create a reactor network
+reactor_network = bklabct.ReactorNet([reactor])
+
+ratelist=[]
+# Advance the simulation to steady state
+time = 0.0
+end_time = 1  # Set an end time for the simulation
+while time < end_time:
+    time = reactor_network.step()
+
+    # Output the rate constant
+    rate_constant = reactor.kinetics.forward_rate_constants[0]
+    ratelist.append(rate_constant)
+print(ratelist)
+
+
+
+
