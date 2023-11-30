7716350678b908d7ce3f68e22750349b1f778341
Patrick Singal <pjs2206@columbia.edu>
2023-11-30 14:43:04 -0500

    update

--- validation.py
+++ validation.py
@@ -9,8 +9,10 @@ bklabct.print_stack_trace_on_segfault()
 
 def printRateConstant(Temp,Pres,X) :
     # file = 'test/data/kineticsfromscratch_LMRtest.yaml'
-    file = 'test/data/alzuetamechanism_LMRR.yaml'
-    reactions = ['H + O2 (+M) <=> HO2 (+M)']
+    # file = 'test/data/alzuetamechanism_LMRR.yaml'
+    file = 'test/data/sandbox.yaml'
+    # reactions = ['H + O2 (+M) <=> HO2 (+M)']
+    reactions = ['NH2 + NH2 (+M) <=> N2H4 (+M)']
     gas = bklabct.Solution(file)
     for i, R in enumerate(reactions):
         k_list=[]
@@ -24,12 +26,15 @@ def printRateConstant(Temp,Pres,X) :
             print(("%.5e      %s")%(k_list[j][0],str(X)))
 
 
-X_list = [{'AR':1.0,'H2O':0.0},{'AR':0.7,'H2O':0.3},{'AR':0.0,'H2O':1.0},
-          {'AR':1.0,'N2':0.0},{'AR':0.7,'N2':0.3},{'AR':0.0,'N2':1.0},
-          {'AR':1.0,'H2':0.0},{'AR':0.7,'H2':0.3},{'AR':0.0,'H2':1.0},
-          {'AR':1.0,'CO2':0.0},{'AR':0.7,'CO2':0.3},{'AR':0.0,'CO2':1.0},
-          {'AR':1.0,'NH3':0.0},{'AR':0.7,'NH3':0.3},{'AR':0.0,'NH3':1.0},
-          {'AR':1.0,'H2O2':0.0},{'AR':0.7,'H2O2':0.3},{'AR':0.0,'H2O2':1.0},
-          {'HE':1.0,'AR':0.0}]
-for X in X_list:
-    printRateConstant([2000],[2*101325],X)
+# X_list = [{'AR':1.0,'H2O':0.0},{'AR':0.7,'H2O':0.3},{'AR':0.0,'H2O':1.0},
+#           {'AR':1.0,'N2':0.0},{'AR':0.7,'N2':0.3},{'AR':0.0,'N2':1.0},
+#           {'AR':1.0,'H2':0.0},{'AR':0.7,'H2':0.3},{'AR':0.0,'H2':1.0},
+#           {'AR':1.0,'CO2':0.0},{'AR':0.7,'CO2':0.3},{'AR':0.0,'CO2':1.0},
+#           {'AR':1.0,'NH3':0.0},{'AR':0.7,'NH3':0.3},{'AR':0.0,'NH3':1.0},
+#           {'AR':1.0,'H2O2':0.0},{'AR':0.7,'H2O2':0.3},{'AR':0.0,'H2O2':1.0},
+#           {'HE':1.0,'AR':0.0}]
+# for X in X_list:
+#     printRateConstant([2000],[2*101325],X)
+
+P_list=np.multiply([]
+printRateConstant()
