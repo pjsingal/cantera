f65c2c51028cf2e3a238f3529eb15bb63f93ecec
pjsingal <pjs2206@columbia.edu>
2024-09-10 12:46:28 -0400

    better version with better exp agreement

--- burkelab_SimScripts/simulateflameresidencetime.py
+++ burkelab_SimScripts/simulateflameresidencetime.py
@@ -4,74 +4,122 @@ import cantera as ct
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import pandas as pd
 mpl.rc('font',family='Times New Roman')
 
 save_fig = True
 nominal_model = 'G:\\Mon disque\\Columbia\\Burke Lab\\07 Mechanisms\\Ammonia\\Mei-2019\\mei-2019.yaml'
-initial_temp = 300
+# initial_temp = 300
 
 P = 1e5 # Pa
+obs = 'NO'
+width = 2
 
-def gas(T,P,X):
+def cp(T,P,X):
     gas_stream = ct.Solution(nominal_model)
-    gas_stream.TPX = T, P, X
-    return gas_stream
+    gas_stream.TPX = T, P, {X:1}
+    return gas_stream.cp_mole # [J/kmol/K]
 
-fuel = gas(300,P,{'NH3':1})
-air = gas(650,P,{'N2':0.79,'O2':0.21})
+
+T_fuel = 300
+T_air = 650
+cp_fuel = cp(T_fuel,P,'NH3') # [J/kmol/K]
+cp_o2 = cp(T_air,P,'O2') # [J/kmol/K]
+cp_n2 = cp(T_air,P,'N2') # [J/kmol/K]
 
 phi = 1.22
+x_fuel = (phi*(1/0.75)*0.21)/(1+phi*(1/0.75)*0.21)
+x_o2 = 0.21*(1-x_fuel)
+x_n2 = 0.79*(1-x_fuel)
+x_air=1-x_fuel
+T_mix = (x_fuel*cp_fuel*T_fuel+(x_o2*cp_o2+x_n2*cp_n2)*T_air)/(x_fuel*cp_fuel+ x_o2*cp_o2 + x_n2*cp_n2)
+# print(T_mix)
 
-# Calculate the moles of ammonia and air needed for the desired equivalence ratio
-# For phi = (n_fuel/n_air) / (n_fuel/n_air)stoic
-n_fuel = 1.0  # Assume 1 mole of ammonia
-n_air_stoic = 4.76  # Stoichiometric air requirement for ammonia combustion (3/2 * O2 + 3.76N2)
-n_air = n_air_stoic / phi
-
-mixture = ct.Mixture([(fuel, n_fuel), (air, n_air)])
-mixture_equivalence_ratio = mixture.equivalence_ratio(gas_ammonia, gas_air)
-print(f"Mixture equivalence ratio: {mixture_equivalence_ratio}")
+# def h(T,P,X):
+#     gas_stream = ct.Solution(nominal_model)
+#     gas_stream.TPX = T, P, {X:1}
+#     return gas_stream.enthalpy_mole 
 
 
-T_f = 400  
-pressure = 1
-width = 0.3
-X = 'H2:0.5, O2:0.5, N2:1.549'
-obs = 'NO'
-########################################################################################################################################
+# T_fuel = 330
+# T_air = 650
+# h_fuel = h(T_fuel,P,'NH3')
+# h_o2 = h(T_air,P,'O2')
+# h_n2 = h(T_air,P,'N2')
 
-########################################################################################################################################
-ini_cond = (initial_temp, pressure * ct.one_atm, X)
-gas = ct.Solution(nominal_model)
-gas.TPX = ini_cond
-flame = ct.FreeFlame(gas)
+# phi = 1.22
+# x_fuel = (phi*(1/0.75)*0.21)/(1+phi*(1/0.75)*0.21)
+# # print(x_fuel)
+# x_o2 = 0.21*(1-x_fuel)
+# x_n2 = 0.79*(1-x_fuel)
+# x_air=1-x_fuel
+# print(h_fuel)
+# print(h_o2)
+# print(h_n2) 
+# h_mix = x_fuel*h_fuel + x_o2*h_o2 + x_n2*h_n2
+# print(h_mix)
+# gas = ct.Solution(nominal_model)
+# # gas.HPX = 300,P,{'NH3':x_fuel,'O2':x_o2,'N2':x_n2}
+# gas.HP = h_mix,P
+# print(gas.T)
+
+
+
+# T_mix = (x_fuel*cp_fuel*T_fuel+(x_o2*cp_o2+x_n2*cp_n2)*T_air)/(x_fuel*cp_fuel+ x_o2*cp_o2 + x_n2*cp_n2)
+# # print(T_mix)
+
+# # T_explosion = 1000 # combustor exit temperature
+
+mix1 = ct.Solution(nominal_model)
+mix1.TPX = T_mix, P,{'NH3':x_fuel,'O2':x_o2,'N2':x_n2}
+
+# mix1.equilibrate("HP")
+
+
+flame = ct.FreeFlame(mix1,width=width)
 flame.set_refine_criteria(ratio=3, slope=0.1, curve=0.1)
-flame.solve(loglevel=0, auto=True)         
-T_list = flame.T
-P = flame.P
-velocity_list = flame.velocity
-mole = flame.X
-distance_list = flame.grid
-S_b = list(velocity_list)[-1]
-full_time = [(distance)/S_b for distance in distance_list]
-distance_index = min(range(len(T_list)), key=lambda i: abs(T_list[i]-T_f))
-tau_f = np.array(distance_list[distance_index]) / S_b
-tau_list = [ft - tau_f for ft in full_time] 
-distance_f = np.array(distance_list[distance_index])
-distance_list_new = [dl - distance_f for dl in distance_list]                 
-########################################################################################################################################
+flame.solve(loglevel=0, auto=True)
+state = {
+    'T': flame.T,
+    'P': flame.P,
+    'vel': flame.velocity,
+    'X': flame.X,
+    'd': flame.grid #list of distances [m]
+}
+S_b = list(state['vel'])[-1] # [m/s]
+Rjoule = 8.31446261815324 # [J/K/mol]
+full_time = [(distance)/S_b for distance in state['d']] # [s]
+conc = np.multiply(state['X'][mix1.species_names.index("NH2")], np.divide(P,np.multiply(Rjoule,state['T'])))
+distance_index=np.argmax(conc)
+tau_f = np.array(state['d'][distance_index])/S_b
+tau_list = [(ft - tau_f)*1000 for ft in full_time] # [ms]
+X_NO = state['X'][mix1.species_names.index("NO")]
+X_O2 = state['X'][mix1.species_names.index("O2")]
+X_O2dry = 0.15 # 15% O2 dry
+X_NO_15O2dry = np.multiply(np.multiply(X_NO,np.divide(0.21-X_O2dry,np.subtract(0.21,X_O2))),1e6) # [ppm, 15% O2 dry]
+# distance_f = np.array(state['d'][distance_index])
+# distance_list_new = [dl - distance_f for dl in state['d']]
 
 ########################################################################################################################################
 plt.figure()
-plt.plot(np.multiply(tau_list,1000), np.multiply(mole[gas.species_names.index(obs)],1e6), linestyle='solid', linewidth=3)
+plt.loglog(tau_list, X_NO_15O2dry, linestyle='solid', linewidth=3)
+# sims = pd.DataFrame(np.multiply(tau_list,1000), np.multiply(state['X'][mix1.species_names.index(obs)],1e6))
+# print(sims)
+path="G:\\Mon disque\\Columbia\\Burke Lab\\09 NOx Mini-Project\\Graph Reading\\"
+bar1_data = pd.read_csv(path+'1bar.csv',header=None)
+
+plt.loglog(bar1_data.iloc[:, 0],bar1_data.iloc[:, 1],marker='o',fillstyle='none',linestyle='none',color='k',markersize=3,markeredgewidth=1, label="Gubbi et al.", zorder=110)
+# plt.loglog(np.multiply(full_time,1000), np.multiply(state['X'][mix1.species_names.index(obs)],1e6), linestyle='solid', linewidth=3)
+# plt.loglog(np.multiply(full_time,1000), state['T'], linestyle='solid', linewidth=3)
+# print(bar1_data.iloc[:, :2])
 plt.xlabel('Time [ms]')
+plt.ylim([0.1,5000])
+# plt.xlim([0.01,1350])
 plt.ylabel(obs + ' Mole Fraction [ppm]')
 plt.tick_params(axis = 'x', direction='in')
 plt.tick_params(axis='x',labelsize=15)
 plt.tick_params(axis = 'y', direction='in')
 plt.tick_params(axis='y',labelsize=15)          
-if save_fig == True:        
-    plt.savefig('flame_' + obs + '.pdf',dpi=1000, bbox_inches='tight')
-    plt.savefig('flame_' + obs + '.png',dpi=1000, bbox_inches='tight')        
-plt.show()
-########################################################################################################################################
+plt.savefig('flame_' + obs + '.pdf',dpi=1000, bbox_inches='tight')
+plt.savefig('flame_' + obs + '.png',dpi=1000, bbox_inches='tight')
+print("Successful simulation!")
